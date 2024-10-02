5fe56ea321870be2faf4a1a2c40ec86128705343
natalie-richards <nrichards.sb.31@gmail.com>
2024-10-02 09:48:14 -0700

    🎉 Initial UI commit - upload form, sveltestrap styles

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Styles, Navbar, Nav, NavItem, NavLink } from '@sveltestrap/sveltestrap';
+</script>
+
+<Styles />
+<Navbar color="light" light expand="md">
+	<Nav class="ms-auto" navbar>
+		<NavItem>
+			<NavLink href="#components/">Logout</NavLink>
+		</NavItem>
+	</Nav>
+</Navbar>
+<slot />
